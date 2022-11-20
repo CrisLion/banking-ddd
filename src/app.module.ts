777bd5ba61f53bf17d5ccd723dd1a52cd9f70f00
Cristian Quito <cristianandresquito@gmail.com>
2022-11-20 16:10:57 -0500

    Estado inicial con un cambio en el path de la base de datos

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: "mysql",
+    url: 'mysql://root:puercoconalas7350@@localhost:3306/dockseeker-ddd',
+    migrationsRun: true,
+    logging: true,
+    timezone: '+00:00',
+    bigNumberStrings: false,
+    entities: [
+        process.env.ENVIRONMENT == 'prod' ? 
+        '**/infrastructure/persistence/entities/*{.ts,.js}' : 
+        'dist/**/infrastructure/persistence/entities/*{.ts,.js}'
+    ],
+    subscribers: [],
+    migrations: [
+        process.env.ENVIRONMENT == 'prod' ? 
+        'shared/infrastructure/persistence/migrations/*{.ts,.js}' :
+        'dist/shared/infrastructure/persistence/migrations/*{.ts,.js}'
+    ],
+    migrationsTableName: "migrations"
+    })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
+
