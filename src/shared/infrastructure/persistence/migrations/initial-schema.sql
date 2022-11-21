c538c271d6cccec33c6540c1d69f50c3c69ff3ba
Cristian Quito <cristianandresquito@gmail.com>
2022-11-21 12:17:45 -0500

    Se agrego tablas referentes a dockseeker

--- src/shared/infrastructure/persistence/migrations/initial-schema.sql
+++ src/shared/infrastructure/persistence/migrations/initial-schema.sql
@@ -1,101 +1,36 @@
-CREATE TABLE IF NOT EXISTS departments (
+CREATE TABLE IF NOT EXISTS countries (
   id VARCHAR(2) NOT NULL,
   name VARCHAR(45) NOT NULL,
   PRIMARY KEY(id),
-  UNIQUE KEY UQ_departments_name(name)
+  UNIQUE KEY UQ_countries_name(name)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS provinces (
+CREATE TABLE IF NOT EXISTS states (
   id VARCHAR(4) NOT NULL,
   name VARCHAR(45) NOT NULL,
-  department_id VARCHAR(2) NOT NULL,
+  country_id VARCHAR(2) NOT NULL,
   PRIMARY KEY(id),
-  UNIQUE KEY UQ_provinces_name_department_id(name, department_id),
-  KEY IX_provinces_department_id(department_id),
-  CONSTRAINT FK_provinces_department_id FOREIGN KEY(department_id) REFERENCES departments(id)
+  UNIQUE KEY UQ_states_name_country_id(name, country_id),
+  KEY IX_states_country_id(country_id),
+  CONSTRAINT FK_states_country_id FOREIGN KEY(country_id) REFERENCES countries(id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS districts (
+CREATE TABLE IF NOT EXISTS cities (
   id VARCHAR(6) NOT NULL,
   name VARCHAR(45) NOT NULL,
-  province_id VARCHAR(4) NOT NULL,
+  state_id VARCHAR(4) NOT NULL,
   PRIMARY KEY(id),
-  UNIQUE KEY UQ_districts_name_province_id(name, province_id),
-  KEY IX_districts_province_id(province_id),
-  CONSTRAINT FK_districts_province_id FOREIGN KEY(province_id) REFERENCES provinces(id)
+  UNIQUE KEY UQ_cities_name_state_id(name, state_id),
+  KEY IX_cities_state_id(state_id),
+  CONSTRAINT FK_cities_state_id FOREIGN KEY(state_id) REFERENCES states(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS users (
-  id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  email VARCHAR(255) NOT NULL,
-  district_id VARCHAR(6) NOT NULL,
+CREATE TABLE IF NOT EXISTS streets (
+  id VARCHAR(8) NOT NULL,
+  name VARCHAR(45) NOT NULL,
+  city_id VARCHAR(2) NOT NULL,
   PRIMARY KEY(id),
-  UNIQUE KEY UQ_users_email(email),
-  KEY IX_users_district_id(district_id),
-  CONSTRAINT FK_users_district_id FOREIGN KEY(district_id) REFERENCES districts(id)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE IF NOT EXISTS clients(
-  id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  type ENUM ('C', 'P') NOT NULL DEFAULT 'C',
-  created_at DATETIME NULL,
-  created_by BIGINT UNSIGNED NULL,
-  updated_at DATETIME NULL,
-  updated_by BIGINT UNSIGNED NULL,
-  company_name VARCHAR(150) NULL,
-  ruc VARCHAR(11) NULL,
-  first_name VARCHAR(75) NULL,
-  last_name VARCHAR(75) NULL,
-  dni VARCHAR(8) NULL,
-  PRIMARY KEY(id),
-  UNIQUE INDEX UQ_clients_ruc(ruc),
-  UNIQUE INDEX UQ_clients_company_name(company_name), 
-  UNIQUE INDEX UQ_clients_dni(dni),
-  KEY IX_clients_created_by(created_by),
-  KEY IX_clients_updated_by(updated_by),
-  CONSTRAINT FK_clients_created_by FOREIGN KEY(created_by) REFERENCES users(id),
-  CONSTRAINT FK_clients_updated_by FOREIGN KEY(updated_by) REFERENCES users(id)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE IF NOT EXISTS accounts(
-  id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  number VARCHAR(15) NOT NULL,
-  balance DECIMAL(10,2) NULL,
-  currency VARCHAR(3) NULL,
-  client_id BIGINT UNSIGNED NOT NULL,
-  created_at DATETIME NULL,
-  created_by BIGINT UNSIGNED NULL,
-  updated_at DATETIME NULL,
-  updated_by BIGINT UNSIGNED NULL,
-  PRIMARY KEY (id),
-  UNIQUE KEY UQ_accounts_number(number),
-  KEY IX_accounts_client_id(client_id),
-  KEY IX_accounts_created_by(created_by),
-  KEY IX_accounts_updated_by(updated_by),
-  CONSTRAINT FK_accounts_client_id FOREIGN KEY(client_id) REFERENCES clients(id),
-  CONSTRAINT FK_accounts_created_by FOREIGN KEY(created_by) REFERENCES users(id),
-  CONSTRAINT FK_accounts_updated_by FOREIGN KEY(updated_by) REFERENCES users(id)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE IF NOT EXISTS transactions(
-  id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  type CHAR(1) NOT NULL,
-  status TINYINT UNSIGNED NOT NULL,
-  from_account_id BIGINT UNSIGNED NOT NULL,
-  to_account_id BIGINT UNSIGNED NULL,
-  amount DECIMAL(10,2) NULL,
-  currency VARCHAR(3) NULL,
-  created_at DATETIME NULL,
-  created_by BIGINT UNSIGNED NULL,
-  updated_at DATETIME NULL,
-  updated_by BIGINT UNSIGNED NULL,
-  PRIMARY KEY (id),
-  KEY IX_transactions_from_account_id(from_account_id),
-  KEY IX_transactions_to_account_id(to_account_id),
-  KEY IX_transactions_created_by(created_by),
-  KEY IX_transactions_updated_by(updated_by),
-  CONSTRAINT FK_transactions_from_account_id FOREIGN KEY(from_account_id) REFERENCES accounts(id),
-  CONSTRAINT FK_transactions_to_account_id FOREIGN KEY(to_account_id) REFERENCES accounts(id),
-  CONSTRAINT FK_transactions_created_by FOREIGN KEY(created_by) REFERENCES users(id),
-  CONSTRAINT FK_transactions_updated_by FOREIGN KEY(updated_by) REFERENCES users(id)
+  UNIQUE KEY UQ_streets_name_city_id(name, city_id),
+  KEY IX_streets_city_id(city_id),
+  CONSTRAINT FK_streets_city_id FOREIGN KEY(city_id) REFERENCES cities(id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
